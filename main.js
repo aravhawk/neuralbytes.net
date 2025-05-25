bab88538e9285b9704682a8d4c9c5dc2a6408282
Arav Jain <aravhawk@gmail.com>
2025-05-24 21:41:21 -0500

    tagline change: 'breakthrough' --> 'unique'

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function open_neuralbytes_search() {
 }
 
 function typewriter_header() {
-    const main_page_header_text = "Integrating AI to create breakthrough tools and experiences";
+    const main_page_header_text = "Integrating AI to create unique tools and experiences";
     const typewriterElement = document.getElementById('main_page_header');
     let main_page_header_text_chunk = "";
     let i = 0;
